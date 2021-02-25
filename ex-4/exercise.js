9951a97def26fde3af20af053c19edfae736b34f
Muhammad rRehman <muhammadislamian@gmail.com>
2021-02-25 16:00:14 +0000

    done with ex 4

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,2 +1,10 @@
-// Example
-window.alert("Hello World!");
+var name = prompt("Put your name Please");
+window.alert ("Hello Mr "+name+ " How is  your day today Feeling");
+
+var mood = prompt ("enter your mood");
+
+if(mood === "good"){ 
+    window.alert ("I'm happy for you champ")
+} else if(mood === "bad"){
+    window.alert("That's too bad, tomorrow it's a new day");
+}
