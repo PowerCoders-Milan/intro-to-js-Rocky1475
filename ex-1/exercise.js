b8210662c8af6742d5c255c44ff83ca1e422cffb
Muhammad rRehman <muhammadislamian@gmail.com>
2021-02-25 13:11:59 +0000

    ex1 completed

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,2 +1,8 @@
 // Example
-window.alert("Hello World!");
+
+var children = 3;
+var partner = "John";
+var loca = "Italy";
+var job = "waiter";
+  
+  window.alert ("you have " + children + " kids  and your partners name is " + partner + " and you live in " +loca+ " and your job is " +job)
