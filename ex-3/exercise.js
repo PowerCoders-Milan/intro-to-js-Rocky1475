9f2254304a40cd03a70612f56767253fcf4414de
Muhammad rRehman <muhammadislamian@gmail.com>
2021-02-25 14:06:13 +0000

    ex3 doing

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,13 @@
 // Example
 window.alert("Hello World!");
+var valueone = 10
+var valuetwo = 8
+if (valueone > valuetwo) {
+    window.alert (" the greater number of " + valueone + "and" + valuetwo + "is" valueone);
+}
+else if (valuetwo > valueone) {
+    window.alert ("the greater number of " + valueone + "and" + valuetwo + "is" valuetwo);
+}
+else {
+    window.alert ("the numbers are the same");
+} 
