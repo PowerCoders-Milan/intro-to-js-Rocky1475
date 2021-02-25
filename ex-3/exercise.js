2112ab33a4a870fb9299ea4dea0b51b52fa8c07d
Muhammad rRehman <muhammadislamian@gmail.com>
2021-02-25 14:53:41 +0000

    ex 3 done finally

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,13 +1,12 @@
-// Example
-window.alert("Hello World!");
-var valueone = 10
-var valuetwo = 8
+
+var valueone = prompt("insert the first number please");
+var valuetwo = prompt("insert the second number please");
 if (valueone > valuetwo) {
-    window.alert (" the greater number of " + valueone + "and" + valuetwo + "is" valueone);
+    window.alert ("The first number "+ valueone +" is greater than "+ valuetwo +".");
 }
-else if (valuetwo > valueone) {
-    window.alert ("the greater number of " + valueone + "and" + valuetwo + "is" valuetwo);
+else if (valuetwo > valueone){
+    window.alert ("the second number "+valuetwo+" is greater than "+valueone+".");
 }
 else {
-    window.alert ("the numbers are the same");
-} 
+    window.alert ("the numbers are equal")
+}
